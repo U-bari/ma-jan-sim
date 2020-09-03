ed540ce93d263e5052339d121ee9e814db01905a
U-bari <ken.tanakaja@gmail.com>
2020-09-03 17:11:07 +0900

    init commit

---
+++ Msoft/data.py
@@ -0,0 +1,36 @@
+
+import os
+import csv
+
+
+#data読み込み
+with open('M 2019-10.csv') as f:
+	reader = csv.reader(f)
+	data_2019_10 = [x for x in reader]
+
+with open('M 2019-11.csv') as f:
+	reader = csv.reader(f)
+	data_2019_11 = [x for x in reader]
+	
+with open('M 2019-12.csv') as f:
+	reader = csv.reader(f)
+	data_2019_12 = [x for x in reader]
+
+with open('M 2020-1.csv') as f:
+	reader = csv.reader(f)
+	data_2020_1 = [x for x in reader]
+
+with open('M 2020-2.csv') as f:
+	reader = csv.reader(f)
+	data_2020_2 = [x for x in reader]
+
+with open('M 2020-3.csv') as f:
+	reader = csv.reader(f)
+	data_2020_3 = [x for x in reader]
+
+with open('M 2020-6.csv') as f:
+	reader = csv.reader(f)
+	data_2020_6 = [x for x in reader]
+
+data_list = [data_2019_10, data_2019_11, data_2019_12, data_2020_1, data_2020_2, data_2020_3, data_2020_6]
+time = ["2019_10","2019_11","2019_12","2020_1","2020_2","2020_3","2020_6"]
---
+++ Msoft/test.py
@@ -0,0 +1,25 @@
+
+import os
+import csv
+import rate_system
+import data
+
+
+
+data_list = data.data_list
+
+#初期rate生成
+rate = {}
+member = set([])
+
+for i in data_list:
+	member = rate_system.member_make(i,member)
+
+rate = rate_system.make_rate(member,rate)
+
+#レーティング
+for i in data_list:
+	rate = rate_system.rating(i,rate)
+
+for name, point in rate.items():
+	print(name + ":" + str(point))
